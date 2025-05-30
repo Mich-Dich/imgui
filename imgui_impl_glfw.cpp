8c43cfec07bde18f9b6794ad69f7ada4fbc9e6f0
Mich <mally.dominik@gmx.de>
2025-05-30 10:13:30 +0200

    change title for window manager ignoring

--- imgui_impl_glfw.cpp
+++ imgui_impl_glfw.cpp
@@ -1046,7 +1046,7 @@ static void ImGui_ImplGlfw_CreateWindow(ImGuiViewport* viewport)
     glfwWindowHint(GLFW_FLOATING, (viewport->Flags & ImGuiViewportFlags_TopMost) ? true : false);
 #endif
     GLFWwindow* share_window = (bd->ClientApi == GlfwClientApi_OpenGL) ? bd->Window : nullptr;
-    vd->Window = glfwCreateWindow((int)viewport->Size.x, (int)viewport->Size.y, "PFF_sub_window", nullptr, share_window);
+    vd->Window = glfwCreateWindow((int)viewport->Size.x, (int)viewport->Size.y, "GLT_sub_window", nullptr, share_window);
     vd->WindowOwned = true;
     viewport->PlatformHandle = (void*)vd->Window;
 #ifdef _WIN32
@@ -1181,7 +1181,7 @@ static void ImGui_ImplGlfw_SetWindowSize(ImGuiViewport* viewport, ImVec2 size)
 static void ImGui_ImplGlfw_SetWindowTitle(ImGuiViewport* viewport, const char* title)
 {
     ImGui_ImplGlfw_ViewportData* vd = (ImGui_ImplGlfw_ViewportData*)viewport->PlatformUserData;
-    std::string newTitle = "PFF - ";                                                                    // Prepend "PFF - " to the title
+    std::string newTitle = "GLT - ";                                                                    // Prepend "GLT - " to the title
     newTitle += title;
     glfwSetWindowTitle(vd->Window, newTitle.c_str());
 }
